78b1c97e5fe5b51a1e9f094aca141ae3747a2483
unknown <Moises-Sousa0>
2026-05-17 19:16:54 -0300

    adicionando listar marcas e listar marca especifica

--- app/routers/marcas.py
+++ app/routers/marcas.py
@@ -1,16 +1,16 @@
 from datetime import datetime
-from fastapi import APIRouter #agrupa rotas do mesmo recurso
+from fastapi import APIRouter, HTTPException #agrupa rotas do mesmo recurso
 from pydantic import BaseModel #permite o fastapi validar automaticamente os dados recebidos
 
 router = APIRouter() #cria um instancia da lib apirouter
-
-class MarcaCreate(BaseModel):
+ 
+class MarcaCreate(BaseModel): #pydanticacho
     nome: str
     descricao: str
 
 marcas = []
 
-@router.post("/marcas") #o @ é um decorator, ele basicamente diz "quando chegar um requisi POST em /marcas execute essa funcao:"
+@router.post("/marcas") #o @ é um decorator, ele basicamente diz "quando chegar um request POST em /marcas execute essa funcao:"
 def criar_marca(marca: MarcaCreate): #parametro marca pede o padrao da classe marcacreate, nome e descric
     nova_marca = {
         "id": len(marcas) + 1,
@@ -20,3 +20,19 @@ def criar_marca(marca: MarcaCreate): #parametro marca pede o padrao da classe ma
     }
     marcas.append(nova_marca)
     return nova_marca
+
+#listar todas as marcas
+@router.get("/marcas")
+def ver_marcas():
+    return marcas
+
+#listar marca especifica
+@router.get("/marcas/{id}")
+def buscar_marcar(id: int):
+    for i in  marcas:
+        if i["id"] == id:
+            return i
+            
+    else:
+        raise HTTPException(status_code=404, detail="Marca não encontrada :(") #forma correta de retornar pro fastapi um erro
+    
