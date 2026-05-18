4e032a495bf3c955956ada34a08001e73c37461a
unknown <Moises-Sousa0>
2026-05-17 21:30:58 -0300

    oh meudeus

--- app/database.py
+++ app/database.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Fungo@087@localhost:5432/outrdrop"
+DATABASE_URL = "postgresql://postgres:@localhost:5432/outrdrop"
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base
+Base = (declarative_base)
