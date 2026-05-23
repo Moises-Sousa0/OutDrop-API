958d6d647c992986c865389695a843b4adc5c9d6
unknown <Moises-Sousa0>
2026-05-23 19:14:02 -0300

    feito  put lancamentos

--- app/routers/lancamentos.py
+++ app/routers/lancamentos.py
@@ -22,11 +22,15 @@ def ver_lancamento(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Lançamento não encontrado :(")
     return listar_lanc
 
+
 #criar lancamento
 @router.post("/lancamentos", status_code=201, response_model=schemas.LancamentoResponse)
 def criar_lancamento( lancamento: schemas.LancamentoCreate, usuario_id = Depends(auth.verificar_token), db: Session = Depends(get_db)):
     atribuir_lancamento = db.query(models.Usuario).filter(models.Usuario.id == usuario_id).first()
     produto_ver = db.query(models.Produto).filter(models.Produto.id == lancamento.produto_id).first()
+
+    if produto_ver is None:
+        raise HTTPException(status_code=404, detail="Produto não encontrado ou não existe")
 
     if atribuir_lancamento.marca_id != produto_ver.marca_id:
         raise HTTPException(status_code=403, detail="não é permitido criar um lancamento sem ser o dono da marca")
@@ -42,3 +46,32 @@ def criar_lancamento( lancamento: schemas.LancamentoCreate, usuario_id = Depends
     db.commit()
     db.refresh(novo_lancamento)
     return novo_lancamento
+
+
+#atualizar lancamento
+@router.put("/lancamentos/{id}", status_code=200, response_model=schemas.LancamentoResponse)
+def atualizar_lancamento(id: int, lancamento: schemas.LancamentoUpdate, usuario_id = Depends(auth.verificar_token), db: Session =  Depends(get_db)):
+    verificar_lancamento = db.query(models.Lancamento).filter(models.Lancamento.id == id).first()
+    
+    if verificar_lancamento is None:
+        raise HTTPException(status_code=404, detail="Esse lancamento não existe")
+    
+    verificar_autenticacao = db.query(models.Usuario).filter(models.Usuario.marca_id == verificar_lancamento.marca_id, models.Usuario.id == usuario_id).first()
+    if verificar_autenticacao is None:
+        raise HTTPException(status_code=403, detail=f"{verificar_lancamento.nome} não pertence ao seu usuario")
+    
+    campos_novos = lancamento.model_dump(exclude_unset=True)
+
+    if "produto_id" in campos_novos:
+        verificar_produto = db.query(models.Produto).filter(models.Produto.id == campos_novos["produto_id"]).first()
+        if verificar_produto is None:
+            raise HTTPException(status_code=404, detail="Esse produto não existe")
+        if verificar_produto.marca_id != verificar_autenticacao.marca_id:
+            raise HTTPException(status_code=403, detail="Produto não pertence a essa marca")
+
+    for campo, valor in campos_novos.items():
+        setattr(verificar_lancamento, campo, valor)
+
+    db.commit()
+    db.refresh(verificar_lancamento)
+    return verificar_lancamento
